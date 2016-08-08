5bf110a65a52c16998fe04bfc262f27f048f8c9d
David Azouz <david.azouz@ad.aie.edu>
2016-08-08 18:11:30 +1000

    + Scoreboard buttons + Jumping added

--- Assets/Scripts/UI/WindowManager.cs
+++ Assets/Scripts/UI/WindowManager.cs
@@ -34,7 +34,16 @@ public class WindowManager : MonoBehaviour {
 
     public void Quit()
     {
-        //SceneManager.
+        //If we are running in a standalone build of the game
+#if UNITY_STANDALONE
+        //Quit the application
         Application.Quit();
+#endif
+
+        //If we are running in the editor
+#if UNITY_EDITOR
+        //Stop playing the scene
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
