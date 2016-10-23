b683098aabba5b18195ea2295aff48e01c15d2bd
David Azouz <davidazouz95@hotmail.com>
2016-10-24 00:41:49 +1100

    + 30m Player rings stick to the floor and follow the player (somewhat) while on the shelves + TDD TODO: fix UI when on shelves and set RR fJumpHeight to 2.8 in inspector

--- Assets/Scripts/UI/UIBossLevel.cs
+++ Assets/Scripts/UI/UIBossLevel.cs
@@ -9,6 +9,9 @@ public class UIBossLevel : MonoBehaviour {
 
     public Slider c_BossSlider;
     public Image c_WheelImage;
+    public float fJumpHeight = 2.8f;
+    public float fShelfHeight = 12f;
+    //private Vector3 v3StartingPos;
     private BossBlobs r_BossBlobs;
 
 	// Use this for initialization
@@ -16,7 +19,8 @@ public class UIBossLevel : MonoBehaviour {
     {
         c_BossSlider = GetComponentInChildren<Slider>();
         r_BossBlobs = GetComponentInParent<BossBlobs>();
-
+        fJumpHeight = 2.8f;
+        //v3StartingPos = transform.localPosition;
     }
 	
 	// Update is called once per frame
@@ -25,6 +29,15 @@ public class UIBossLevel : MonoBehaviour {
         // normalise the power value = (between 0 and 1) then 
         // times by ten to make it equal between 0 and 100.
         c_BossSlider.value = 1 + (r_BossBlobs.m_Power - 1) * (100 - 1) / (100 - 1) / 2;
+
+        if (transform.parent.position.y < fJumpHeight)
+        {
+            transform.position = new Vector3(transform.position.x, -0.48f, transform.position.z);
+        }
+        else if (transform.parent.position.y > fShelfHeight)
+        {
+            transform.position = transform.position; // TODO: fix for shelves
+        }
 
         // This is to keep the object the correct rotation without flickering.
         transform.rotation = Quaternion.AngleAxis(-90.0f, Vector3.left);
--- Assets/Scripts/UI/UIDirectionControl.cs
+++ Assets/Scripts/UI/UIDirectionControl.cs
@@ -7,19 +7,35 @@ public class UIDirectionControl : MonoBehaviour
 
     public bool m_UseRelativeRotation = true;       // Use relative rotation should be used for this gameobject?
 
-
+    private Transform m_relativeTransform;
     private Quaternion m_RelativeRotation;          // The local rotatation at the start of the scene.
+    private Vector3 m_RelativePosition;          // The local Position at the start of the scene.
 
 
     private void Start ()
     {
-        m_RelativeRotation = transform.parent.localRotation;
+        m_relativeTransform = transform.parent.transform;
+        m_RelativeRotation = m_relativeTransform.localRotation;
+        m_RelativePosition = m_relativeTransform.position;
     }
 
 
     private void Update ()
     {
         if (m_UseRelativeRotation)
+        {
             transform.rotation = m_RelativeRotation;
+            transform.position = m_RelativePosition;
+        }
+    }
+
+    void OnCollisionEnter(Collision a_collision)
+    {
+        // allow the ring to follow the player when...
+        // they climb up shelves but not when they jump.
+        if (a_collision.transform.tag != "StaticObject")
+        {
+            //transform.localPosition = transform.parent.localPosition;
+        }
     }
 }
